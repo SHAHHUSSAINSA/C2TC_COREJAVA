0a4a5570010bd94966a72a06c042378618958394
shahhussainsa <shahhussainahamed@gmail.com>
2022-11-27 09:40:34 +0530

    second abstract

--- Abstraction/src/com/cg/abstraction/Main.java
+++ Abstraction/src/com/cg/abstraction/Main.java
@@ -1,9 +1,19 @@
 package com.cg.abstraction;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
 
+		String title = s.nextLine();
+
+		Mymovie m = new Mymovie();
+
+		m.setTitle(title);
+
+		System.out.println("The title is:"+m.getTitle());
+
+		}
 	}
-
-}
--- Abstraction/src/com/cg/abstraction/Movie.java
+++ Abstraction/src/com/cg/abstraction/Movie.java
@@ -22,20 +22,8 @@ public abstract class Movie {
 
 	}
 
-
-    public static void main(String[] args) {
-		Scanner s = new Scanner(System.in);
-
-		String title = s.nextLine();
-
-		Mymovie m = new Mymovie();
-
-		m.setTitle(title);
-
-		System.out.println("The title is:"+m.getTitle());
-
-		}
 	}
+    
 
 		
 	
